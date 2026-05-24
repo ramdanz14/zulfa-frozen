3015adf234961bf27fa9949005f1b93bcfb53988
ramdanz14 <ramdanz14@gmail.com>
2026-05-24 21:07:08 +0700

    add toaster init

--- public/assets/js/plugins/toastr-init.js
+++ public/assets/js/plugins/toastr-init.js
@@ -74,7 +74,7 @@
               w(b);
             },
           }),
-          !0)
+            !0)
         );
       }
       function r(c) {
@@ -162,20 +162,20 @@
           e.closeOnHover && j.hover(I, H),
             !e.onclick && e.tapToDismiss && j.click(G),
             e.closeButton &&
-              n &&
-              n.click(function (a) {
-                a.stopPropagation
-                  ? a.stopPropagation()
-                  : void 0 !== a.cancelBubble &&
-                    a.cancelBubble !== !0 &&
-                    (a.cancelBubble = !0),
-                  e.onCloseClick && e.onCloseClick(a),
-                  G(!0);
-              }),
+            n &&
+            n.click(function (a) {
+              a.stopPropagation
+                ? a.stopPropagation()
+                : void 0 !== a.cancelBubble &&
+                a.cancelBubble !== !0 &&
+                (a.cancelBubble = !0),
+                e.onCloseClick && e.onCloseClick(a),
+                G(!0);
+            }),
             e.onclick &&
-              j.click(function (a) {
-                e.onclick(a), G();
-              });
+            j.click(function (a) {
+              e.onclick(a), G();
+            });
         }
         function x() {
           j.hide(),
@@ -185,7 +185,7 @@
               complete: e.onShown,
             }),
             e.timeOut > 0 &&
-              ((h = setTimeout(G, e.timeOut)),
+            ((h = setTimeout(G, e.timeOut)),
               (o.maxHideTime = parseFloat(e.timeOut)),
               (o.hideEta = new Date().getTime() + o.maxHideTime),
               e.progressBar && (o.intervalId = setInterval(J, 10)));
@@ -253,8 +253,8 @@
         function H() {
           (e.timeOut > 0 || e.extendedTimeOut > 0) &&
             ((h = setTimeout(G, e.extendedTimeOut)),
-            (o.maxHideTime = parseFloat(e.extendedTimeOut)),
-            (o.hideEta = new Date().getTime() + o.maxHideTime));
+              (o.maxHideTime = parseFloat(e.extendedTimeOut)),
+              (o.hideEta = new Date().getTime() + o.maxHideTime));
         }
         function I() {
           clearTimeout(h),
@@ -273,8 +273,8 @@
         if (
           ("undefined" != typeof c.optionsOverride &&
             ((e = a.extend(e, c.optionsOverride)),
-            (f = c.optionsOverride.iconClass || f)),
-          !F(e, c))
+              (f = c.optionsOverride.iconClass || f)),
+            !F(e, c))
         ) {
           d++, (b = i(e, !0));
           var h = null,
@@ -300,7 +300,7 @@
       function w(a) {
         b || (b = i()),
           a.is(":visible") ||
-            (a.remove(),
+          (a.remove(),
             (a = null),
             0 === b.children().length && (b.remove(), (g = void 0)));
       }
@@ -330,174 +330,12 @@
     })();
   });
 
-  // Success Type
-  $("#ts-success").on("click", function () {
-    toastr.success("Have fun storming the castle!", "Miracle Max Says");
-  });
-
-  // Success Type
-  $("#ts-info").on("click", function () {
-    toastr.info("We do have the Kapua suite available.", "Turtle Bay Resort");
-  });
-
-  // Success Type
-  $("#ts-warning").on("click", function () {
-    toastr.warning(
-      "My name is Inigo Montoya. You killed my father, prepare to die!"
-    );
-  });
-
-  // Success Type
-  $("#ts-error").on("click", function () {
-    toastr.error(
-      "I do not think that word means what you think it means.",
-      "Inconceivable!"
-    );
-  });
-
-  // Position Top Left
-  $("#pos-top-left").on("click", function () {
-    toastr.info(
-      "I do not think that word means what you think it means.",
-      "Top Left!",
-      { positionClass: "toastr toast-top-left", containerId: "toast-top-left" }
-    );
-  });
-
-  // Position Top Center
-  $("#pos-top-center").on("click", function () {
-    toastr.info(
-      "I do not think that word means what you think it means.",
-      "Top Center!",
-      {
-        positionClass: "toastr toast-top-center",
-        containerId: "toast-top-center",
-      }
-    );
-  });
-
-  // Position Top Right
-  $("#pos-top-right").on("click", function () {
-    toastr.info(
-      "I do not think that word means what you think it means.",
-      "Top Right!",
-      {
-        positionClass: "toastr toast-top-right",
-        containerId: "toast-top-right",
-      }
-    );
-  });
-
-  // Position Top Full Width
-  $("#pos-top-full").on("click", function () {
-    toastr.info(
-      "I do not think that word means what you think it means.",
-      "Top Full Width!",
-      {
-        positionClass: "toastr toast-top-full-width",
-        containerId: "toast-top-full-width",
-      }
-    );
-  });
-
-  // Position Bottom Left
-  $("#pos-bottom-left").on("click", function () {
-    toastr.info(
-      "I do not think that word means what you think it means.",
-      "Bottom Left!",
-      {
-        positionClass: "toastr toast-bottom-left",
-        containerId: "toast-bottom-left",
-      }
-    );
-  });
-
-  // Position Bottom Center
-  $("#pos-bottom-center").on("click", function () {
-    toastr.info(
-      "I do not think that word means what you think it means.",
-      "Bottom Center!",
-      {
-        positionClass: "toastr toast-bottom-center",
-        containerId: "toast-bottom-center",
-      }
-    );
-  });
-
-  // Position Bottom Right
-  $("#pos-bottom-right").on("click", function () {
-    toastr.info(
-      "I do not think that word means what you think it means.",
-      "Bottom Right!",
-      {
-        positionClass: "toastr toast-bottom-right",
-        containerId: "toast-bottom-right",
-      }
-    );
-  });
-
-  // Position Bottom Full Width
-  $("#pos-bottom-full").on("click", function () {
-    toastr.info(
-      "I do not think that word means what you think it means.",
-      "Bottom Full Width!",
-      {
-        positionClass: "toastr toast-bottom-full-width",
-        containerId: "toast-bottom-full-width",
-      }
-    );
-  });
-
-  // Text Notification
-  $("#text-notification").on("click", function () {
-    toastr.info("Have fun storming the castle!", "Miracle Max Says");
-  });
-
-  // Close Button
-  $("#close-button").on("click", function () {
-    toastr.success("Have fun storming the castle!", "With Close Button", {
-      closeButton: true,
-    });
-  });
-
-  // Progress Bar
-  $("#progress-bar").on("click", function () {
-    toastr.warning("Have fun storming the castle!", "Progress Bar", {
-      progressBar: true,
-    });
-  });
-
-  // Clear Toast Button
-  $("#clear-toast-btn").on("click", function () {
-    toastr.error(
-      'Clear itself?<br /><br /><button type="button" class="btn btn-secondary clear">Yes</button>',
-      "Clear Toast Button"
-    );
-  });
-
-  // Slide Down / Slide Up
-  $("#slide-toast").on("click", function () {
-    toastr.success(
-      "I do not think that word means what you think it means.",
-      "Slide Down / Slide Up!",
-      { showMethod: "slideDown", hideMethod: "slideUp", timeOut: 2000 }
-    );
-  });
-
-  // Fade In / Fade Out
-  $("#fade-toast").on("click", function () {
-    toastr.success(
-      "I do not think that word means what you think it means.",
-      "Slide Down / Slide Up!",
-      { showMethod: "fadeIn", hideMethod: "fadeOut", timeOut: 2000 }
-    );
-  });
 })(
   "function" == typeof define && define.amd
     ? define
     : function (a, b) {
-        "undefined" != typeof module && module.exports
-          ? (module.exports = b(require("jquery")))
-          : (window.toastr = b(window.jQuery));
-      }
+      "undefined" != typeof module && module.exports
+        ? (module.exports = b(require("jquery")))
+        : (window.toastr = b(window.jQuery));
+    }
 );
