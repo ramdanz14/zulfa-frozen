f4e8443bdcd31c89b2eddd40f585008f461de2b1
ramdanz14 <ramdanz14@gmail.com>
2026-06-07 20:49:27 +0700

    add locale indonesia

---
+++ app/Language/id/Time.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * This file is part of CodeIgniter 4 framework.
+ *
+ * (c) CodeIgniter Foundation <admin@codeigniter.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+// Time language settings
+return [
+    'invalidFormat'  => '"{0}" datetime format tidak sesuai',
+    'invalidMonth'   => 'Bulan Harus di antara 1 sampai 12. Nilai: {0}',
+    'invalidDay'     => 'hari Harus di antara 1 sampai 31. Nilai. Given: {0}',
+    'invalidOverDay' => 'Days must be between 1 and {0}. Given: {1}',
+    'invalidHours'   => 'Hours must be between 0 and 23. Given: {0}',
+    'invalidMinutes' => 'Minutes must be between 0 and 59. Given: {0}',
+    'invalidSeconds' => 'Seconds must be between 0 and 59. Given: {0}',
+    'years'          => '{0, plural, =1{# tahun} other{# tahun}}',
+    'months'         => '{0, plural, =1{# bulan} other{# bulan}}',
+    'weeks'          => '{0, plural, =1{# minggu} other{# minggu}}',
+    'days'           => '{0, plural, =1{# hari} other{# hari}}',
+    'hours'          => '{0, plural, =1{# jam} other{# jam}}',
+    'minutes'        => '{0, plural, =1{# menit} other{# menit}}',
+    'seconds'        => '{0, plural, =1{# detik} other{# detik}}',
+    'ago'            => '{0} yang lalu',
+    'inFuture'       => 'in {0}',
+    'yesterday'      => 'Kemarin',
+    'tomorrow'       => 'Besok',
+    'now'            => 'Baru Saja',
+];
